2bb73131c59ef97f74ead76cf285d35a8d4bc4e9
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-04-15 15:41:22 +0200

    feat(path): add path module to determine output of generated steps

---
+++ textflow/path.py
@@ -0,0 +1,39 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import utila
+
+import textflow
+
+
+def expected(path: str, prefix: str = '') -> str:
+    return utila.pathconnector(
+        path,
+        textflow.PROCESS,
+        'alignment_expected',
+        prefix,
+    )
+
+
+def alignment(path: str, prefix: str = '') -> str:
+    return utila.pathconnector(
+        path,
+        textflow.PROCESS,
+        'alignment_current',
+        prefix,
+    )
+
+
+def lineending(path: str, prefix: str = '') -> str:
+    return utila.pathconnector(
+        path,
+        textflow.PROCESS,
+        'lineending_lastchar',
+        prefix,
+    )
