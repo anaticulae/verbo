bc5cc8879b04fb37ecd76647764cfdd55b0ccd00
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-04-15 13:06:10 +0200

    test(cli): add command line --help unit test

---
+++ tests/textflow_/test_cli.py
@@ -0,0 +1,14 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import tests.textflow_
+
+
+def test_textflow_cli(monkeypatch):
+    tests.textflow_.run('--help', monkeypatch=monkeypatch)
