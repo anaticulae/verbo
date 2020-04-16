4a8729c2329273d41da11c9df865aecb1acca5d2
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-04-17 01:04:00 +0200

    test(alignment): extend test coverage

--- tests/textflow_/test_cli.py
+++ tests/textflow_/test_cli.py
@@ -7,8 +7,27 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import pytest
+
 import tests.textflow_
 
 
 def test_textflow_cli(monkeypatch):
     tests.textflow_.run('--help', monkeypatch=monkeypatch)
+
+
+def test_textflow_alignments_restruct(testdir, monkeypatch):
+    """Ensure that document with empty page is parsed correctly."""
+    tests.textflow_.run(
+        f'-i {tests.resources.RESTRUCT}',
+        monkeypatch=monkeypatch,
+    )
+
+
+@pytest.mark.parametrize('source', [
+    pytest.param(tests.resources.MASTER72, id='master72'),
+    pytest.param(tests.resources.PYPORTING, id='pyporting'),
+])
+def test_textflow_alignments(source, testdir, monkeypatch):
+    """Ensure that document with empty page is parsed correctly."""
+    tests.textflow_.run(f'-i {source}', monkeypatch=monkeypatch)
