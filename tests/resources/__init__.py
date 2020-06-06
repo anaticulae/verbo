d830b86960d53022231e89638340b8036f4b8420
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-06 23:47:00 +0200

    refactor(remove): remove unused constants

--- tests/resources/__init__.py
+++ tests/resources/__init__.py
@@ -27,22 +27,12 @@ NO_TITLE = os.path.join(GENERATED, 'notitle')
 
 RESTRUCT = os.path.join(GENERATED, 'restruct')
 RESTRUCT_PDF = os.path.join(DOCU, 'restructuredtext.pdf')
-RESTRUCT_CHAPTER_COUNT = 8
-RESTRUCT_TOC_LINES = 13
 
 HOWTO_PYPORTING = os.path.join(GENERATED, 'howto_pyporting')
 HOWTO_PYPORTING_PDF = os.path.join(DOCU, 'howto_pyporting.pdf')
-# the simple example has two 2 chapters, but there are on the same page,
-# therfore 1 page_count.
-# TODO: Change after removed xfail, see: test_sections_extract_sections_simple
-HOWTO_PYPORTING_CHAPTER_PAGE_COUNT = 1  # change to 2
-HOWTO_PYPORTING_HEADLINES_PAGE_3 = 4
-HOWTO_PYPORTING_TOC_LINES = 12
 
-# porting module
 PYPORTING = os.path.join(GENERATED, 'porting_module')
 PYPORTING_PDF = os.path.join(DOCU, 'porting_extension_modules.pdf')
-PYPORTING_CHAPTER_COUNT = 6
 
 BACHELOR37 = os.path.join(GENERATED, 'page_37_tables')
 BACHELOR37_PDF = os.path.join(BACHELOR, 'page_37_tables.pdf')
