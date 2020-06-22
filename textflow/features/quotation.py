051ed620f7ced55c4dea34545fc1fcaaf3455b04
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-22 19:09:49 +0200

    style(format): format code

--- textflow/features/quotation.py
+++ textflow/features/quotation.py
@@ -7,7 +7,6 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-
 import german
 import iamraw
 import serializeraw
