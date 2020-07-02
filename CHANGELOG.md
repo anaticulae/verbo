cc81e2d44825ce4f60ee456277c27e943920520d
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-07-02 19:08:33 +0200

    0.9.9 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v0.9.9
+
 ## v0.9.8
 
 ## v0.9.7
--- words/__init__.py
+++ words/__init__.py
@@ -9,7 +9,7 @@
 
 import os
 
-__version__ = '0.9.8'
+__version__ = '0.9.9'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
