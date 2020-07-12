f5f8318082786a59c0d678e13d9c1b9ede401596
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-07-12 22:50:54 +0200

    feat(utila): add multi dimension near checker

--- words/__patch__.py
+++ words/__patch__.py
@@ -101,3 +101,54 @@ def isnumber(item: str) -> bool:
 
 
 utila.isnumber = isnumber
+
+
+def near_dims(
+        item: tuple,
+        dims: tuple,
+        nears: tuple,
+        allow_none: bool = False,
+) -> bool:
+    """\
+    >>> near_dims((5, 5), [(4, 6), (4, 7), (10, 10)], [(1, 1), (1, 1), (0, 0)])
+    0
+    >>> near_dims((5, 5, 5),
+    ...           dims=[(4, 6, 10), (4, 7, 30), (10, 10, 50)],
+    ...           nears=[(1, 1, 0), (1, 1, 0), (5, 5, 45)])
+    2
+    >>> near_dims((5, 5), [(4,6), (4, 7)], [(0, 0), (0, 0)]) is None
+    True
+    """
+    assert_equal_dim(item, dims)
+    dims = list(dims)
+    nears = list(nears)
+    matches = utila.make_tuple(len(dims))
+    for index, check in enumerate(item):
+        if not dims:
+            return None
+        old_dims, dims = dims, []
+        old_news, nears = nears, []
+        old_matches, matches = matches, []
+        for dim, near_, match in zip(old_dims, old_news, old_matches):
+            if dim[index] is None or check is None and allow_none:
+                # None item always pass the test
+                pass
+            elif not utila.near(dim[index], check, near_[index]):
+                continue
+            dims.append(dim)
+            nears.append(near_)
+            matches.append(match)
+    if matches:
+        matches = matches[0] if len(matches) == 1 else matches
+        return matches
+    return None
+
+
+def assert_equal_dim(item, dims) -> int:
+    unique = {len(item) for item in dims}
+    assert len(unique) == 1, str(unique)
+    expected = unique.pop()
+    assert len(item) == expected, f'{len(item)} != {expected}'
+
+
+utila.near_dims = near_dims
