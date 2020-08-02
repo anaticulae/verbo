606ad73123e5df3ad395174d70f5e3eb06b0b523
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-08-02 23:38:25 +0200

    test(list): add regression test where plus sign was parsed as list item

---
+++ tests/lists/test_regression.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import power
+
+import tests
+
+
+def test_nolist_bachelor241_page81(testdir, monkeypatch):
+    source = power.link(power.BACHELOR241_PDF)
+    tests.run(f'-i {source} --pages=81 --list', monkeypatch=monkeypatch)
