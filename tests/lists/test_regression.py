4af2b32d48d206c5c990ed4216e1ca4291e60336
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-08-02 23:48:13 +0200

    test(list): ensure that table is not parsed as list

--- tests/lists/test_regression.py
+++ tests/lists/test_regression.py
@@ -8,10 +8,19 @@
 # =============================================================================
 
 import power
+import pytest
+import serializeraw
 
 import tests
+import words.path
 
 
+@pytest.mark.xfail(reason='improve list parser')
 def test_nolist_bachelor241_page81(testdir, monkeypatch):
+    """This test was designed cause on this page table content was
+    parsed as lists."""
     source = power.link(power.BACHELOR241_PDF)
     tests.run(f'-i {source} --pages=81 --list', monkeypatch=monkeypatch)
+
+    loaded = serializeraw.load_lists(words.path.lists(testdir.tmpdir))
+    assert not loaded
