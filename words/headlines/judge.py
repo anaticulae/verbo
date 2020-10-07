a698117bafb49cc0ba0e16bdbe5b5c4d4618061b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-10-08 01:02:36 +0200

    feat(headlines): skip extraction result with too many level one headlines

--- words/headlines/judge.py
+++ words/headlines/judge.py
@@ -7,6 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import configo
 import utila
 
 
@@ -15,6 +16,9 @@ def run(results):
         1. Compare Multiline and NoLevel - prefer multiline over NoLevel
         2. Compare result of 1. with StandardHeadlineExtractor
     """
+    # remove invalid result
+    results = [item if not invalid_extraction(item) else [] for item in results]
+
     best = results[1]
     if any([len(item) for item in results[0]]):
         # if any item is detected with multiline strategy, choose
@@ -39,3 +43,21 @@ def score_headlines(items):
             # level
             score += len(item.title)
     return score
+
+
+MAX_LEVELONE_IN_A_ROW = configo.HV_INT_PLUS(4).value
+
+
+def invalid_extraction(headlines) -> bool:
+    """Judge extracted strategy and decide if result can be valid.
+
+    1. Strategy: Check longest sequence of level one headlines.
+    """
+    headlines = utila.flatten(headlines)
+    levels = [item.level for item in headlines if item.level is not None]
+    grouped = utila.groupby_diff(levels, diff=0, sort=False)  # pylint:disable=unexpected-keyword-arg
+
+    longest_levelone = utila.longest([item for item in grouped if item[0] == 1])
+    if len(longest_levelone) > MAX_LEVELONE_IN_A_ROW:  # TODO: HOLY VALUE
+        return True
+    return False
