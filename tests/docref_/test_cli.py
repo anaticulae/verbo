eb1d1f07a8c41a5e4de13e90d03772d1f2a9747e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-11-25 13:56:55 +0100

    test(docref): add basic docref test infrastructure

---
+++ tests/docref_/test_cli.py
@@ -0,0 +1,14 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import tests.docref_
+
+
+def test_docref_help(monkeypatch):
+    tests.docref_.run('--help', monkeypatch=monkeypatch)
