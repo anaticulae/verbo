f32b065ff82892e361f57af00b5edbc46bf7aa80
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-12-11 13:30:08 +0100

    feat(bib): add tech parser facade

---
+++ docref/bibliography/strategies/tech.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import detector.bibliography.label
+import iamraw
+
+
+def parse(item: str) -> iamraw.BibliographyReference:
+    return detector.bibliography.label.parses(item)
