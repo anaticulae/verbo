77a5ce66ad42ccb42e123c9f5cc79248d62fce4e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-01-02 02:58:40 +0100

    feat(headlines): add cluster strategy
    + add unit test

---
+++ tests/headlines/test_headlines_cluster.py
@@ -0,0 +1,49 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import iamraw
+import power
+import serializeraw
+import utila
+
+import words.headlines.machine
+import words.headlines.strategies.cluster
+
+
+def test_headlines_cluster_master155():
+    source = power.link(power.MASTER155_PDF)
+    pages = utila.ranged_tuple(0, 50)
+    ptcns = serializeraw.create_pagetextcontentnavigators_frompath(
+        source,
+        pages=pages,
+    )
+    # TODO: START END DOES NOT MATCH WITH CONTENT. IS THIS A PROBLEM?
+    # SHOULD WE REMOVE THIS REDUNDANCY?
+    result = words.headlines.machine.headlines(
+        ptcns=ptcns,
+        sectionlist=iamraw.Sections(content=[
+            iamraw.sections.Introduction(0, 7, 1.0),
+            iamraw.MainPart(
+                start=7,
+                end=13,
+                trust=1.0,
+                content=[
+                    iamraw.sections.Chapter(7, 13, 1.0),
+                    iamraw.sections.Chapter(14, 20, 1.0),
+                    iamraw.sections.Chapter(21, 28, 1.0),
+                    iamraw.sections.Chapter(29, 47, 1.0),
+                ],
+            ),
+        ]),
+        chapters=[0, 1, 2, 3],
+        strategies=[words.headlines.strategies.cluster],
+        pages=pages,
+    )
+    result = result[0]  # single strategy was used
+    assert len(result) == 5  # five different chapter
---
+++ words/headlines/strategies/cluster.py
@@ -0,0 +1,75 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""\
+Cluster Strategy
+================
+
+Hint: THIS STRATGY INVOCATION IS A HACK TO PASS THE STRATEGY. WE MUST
+      THINK THIS OVER.
+"""
+
+import collections
+
+import doctextstyle.features
+import doctextstyle.features.headline
+import doctextstyle.parser
+import doctextstyle.utils
+import iamraw
+import utila
+
+import words.headlines.strategies.multiline
+
+
+def filter_headlines(parsed) -> dict:  # pylint:disable=R0914
+    flat = utila.flatten([item for item in parsed.values()])
+    flat = doctextstyle.utils.flatten(flat)
+    headlines = doctextstyle.features.headline.headlines(
+        flat,
+        returncluster=True,
+    )
+    if not headlines:
+        utila.error('empty headlines for strategy cluster')
+        return {}
+
+    _, clusters = headlines
+    clusters = [{item.hashed for item in level} for level in clusters]
+
+    result = collections.defaultdict(list)
+    for number, chapter in parsed.items():
+        for page in chapter:
+            for containerid, line in enumerate(page.hashed):
+                current = search_level(line, clusters)
+                if current == -1:
+                    continue
+                if words.headlines.strategies.multiline.noheadline(line):
+                    continue
+                headline = iamraw.Headline(
+                    title=line,
+                    level=current + 1,
+                    page=page.page,
+                    container=containerid,
+                )
+                result[number].append(headline)
+    result = dict(result)  # pylint:disable=R0204
+    return result
+
+
+def extract_page(data, page):
+    ptcn = utila.select_page(data.ptcns, page=page)
+    parsed = doctextstyle.parser.parse(ptcn, magic=[])
+    return [parsed]
+
+
+def search_level(line, clusters):
+    # TODO: ADD MECHANISM TO CHECK IF ITEM IS NEAR TO CLUSTER TO FIND
+    # MORE HEADLINES
+    for index, cluster in enumerate(clusters):
+        if line in cluster:
+            return index
+    return -1
