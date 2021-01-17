f9f2b38245962cd04f5d7c6186f7de3d9f2230e9
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-01-17 09:53:41 +0100

    docs(docref): move bibliography documentation

--- docref/features/bibliography.py
+++ docref/features/bibliography.py
@@ -6,6 +6,59 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+"""Bibliography Link
+=================
+
+This module enables to parse links to the bibliography sources out of text
+flow.
+
+See `footlink`.
+
+Examples
+--------
+
+* [Pap15], [Wik07]
+* (vgl. Havelock 1963: 166), (vgl. Ong 2012: 145).
+
+Styles
+------
+
+Harvard
+~~~~~~~
+
+* (vgl. Havelock 1963: 166)
+* (vgl. Havelock 1986: 77; Robinson/Hawpe 1986: 124)
+* (ebd.: 18; vgl. hierzu auch Havelock 1963: 47)
+* (vgl. Plat. Men.: 97a-98c)
+* (vgl. ebd.: 6; Havelock 1982: 186; Murray/Wilson 2004: 1)
+* (vgl. Dierse 1977: 2-6)
+* (Meier 2007: 192)
+
+Chicago
+~~~~~~~
+
+Like Harvard but located in the footer?
+
+Technical
+~~~~~~~~~
+
+* [WAS19]
+
+Numbers
+~~~~~~~
+
+* (144,13)
+
+Location
+--------
+
+There are 2 location where bibliography links can be located. On the one
+hand there can stand in the floating text to reference sentences or
+pargraph. On the other hand footer can contain list of bibliography links
+
+TODO: FOOTER
+TODO: TECHNICAL
+"""
 
 import iamraw
 import serializeraw
