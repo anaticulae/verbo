7fbaedf6f25151177e2e3f655c3d3e93bc9d4fa9
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-12-06 17:54:17 +0100

    test(abbr): validate bachelor37 abbr

--- tests/abbreviations/test_abbreviation_parse.py
+++ tests/abbreviations/test_abbreviation_parse.py
@@ -9,8 +9,10 @@
 
 import power
 import serializeraw
+import utila
 import utilatest
 
+import tests
 import words.feature.abbreviation
 import words.path
 
@@ -28,6 +30,30 @@ def bachelor37():
 def test_abbreviation_parse_page():
     result = bachelor37()
     assert len(result) > 100, str(result)
+
+
+def abbr(source, pages, testdir, monkeypatch, flat: bool = False):
+    source = power.link(source)
+    cmd = f'-i {source} -o {testdir.tmpdir} --abbreviation --pages={pages}'
+    tests.run(cmd, monkeypatch=monkeypatch)
+    abbrpath = words.path.abbr(testdir.tmpdir)
+    result = serializeraw.load_text_abbreviations(abbrpath)
+    if flat:
+        result = [[item.short for item in page.content] for page in result]
+        result = utila.flatten(result)
+        result = utila.make_unique(result)
+    return result
+
+
+BACHELOR37 = ('PFC al. IAPS US FMRI s. bzw. ENBP EN BP SAM SD= I) II III RS CX '
+              'EKG etc. PASW SPSS IL USA ANOVA SA SEM AG CD MATLAB MA BPM ca. '
+              'F( T( MB PM SD M= MDHF \u2212MD HF Z= PET')
+
+
+def test_bachelor37abbr(testdir, monkeypatch):
+    extracted = abbr(power.BACHELOR037_PDF, '5:33', testdir, monkeypatch, True)
+    raw = ' '.join(extracted)
+    assert raw == BACHELOR37
 
 
 def test_abbreviation_dump_load_parsed_abbreviation():
