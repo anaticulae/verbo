0f77fcbea22213cdf7b3c8a58ee783993020d239
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-12-06 17:35:18 +0100

    test(refactor): simplify test code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,39 +15,40 @@ import utilatest
 import tests
 
 
-@pytest.mark.parametrize('command', [
-    pytest.param(['--help'], id='help'),
-    pytest.param(['--version'], id='version'),
-    pytest.param(
-        ['-i', power.link(power.DOCU027_PDF), '-o', '.'],
-        id='restructured',
-    ),
-    pytest.param(
-        ['-i', power.link(power.DOCU027_PDF), '-o', '.', '--pages', '0:9'],
-        id='pages',
-    ),
-    pytest.param(['-i', power.link(power.MASTER072_PDF)], id='master72'),
-    pytest.param(['-i', power.link(power.DOCU009_PDF)], id='pyporting'),
+@pytest.mark.parametrize('source, pages', [
+    pytest.param(power.DOCU027_PDF, '0:9', id='docu27pages'),
+    pytest.param(power.MASTER072_PDF, None, id='master72'),
+    pytest.param(power.DOCU009_PDF, None, id='pyporting'),
 ])
 @pytest.mark.usefixtures('testdir')
 @utilatest.requires(power.DOCU009_PDF)
 @utilatest.requires(power.DOCU027_PDF)
 @utilatest.requires(power.MASTER072_PDF)
 @utilatest.nightly
-def test_run_words(command, monkeypatch, capsys):
+def test_run_words(source, pages, monkeypatch, capsys):
+    """Run help and version command to reach basic test coverage"""
+    pages = pages if pages else ':'
+    cmd = f'-i {power.link(source)} --pages {pages}'
+    tests.run(cmd, monkeypatch=monkeypatch)
+    utilatest.write_capsys(capsys)
+
+
+@pytest.mark.parametrize('command', [
+    pytest.param('--help', id='help'),
+    pytest.param('--version', id='version'),
+])
+def test_run_words_basic(command, monkeypatch):
     """Run help and version command to reach basic test coverage"""
     tests.run(command, monkeypatch=monkeypatch)
-    utilatest.write_capsys(capsys)
 
 
 @utilatest.nightly
 @utilatest.requires(power.MASTER072_PDF)
 def test_feature_words_work_pages0_10(testdir, monkeypatch):
-    root = str(testdir)
-    cmd = f'-i {root} -o {root} --pages=0:10'
+    cmd = f'-i {testdir.tmpdir} -o {testdir.tmpdir} --pages=0:10'
     utila.copy_content(
         src=power.link(power.MASTER072_PDF),
-        dest=root,
+        dest=testdir.tmpdir,
         pattern='(rawmaker|sections|groupme)__*.yaml',
     )
     tests.run(cmd, monkeypatch=monkeypatch)
