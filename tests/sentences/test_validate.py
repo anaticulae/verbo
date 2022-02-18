0bd6a727540f65fd960d78b837bbb4c0b8c444cf
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-18 01:18:56 +0100

    test(sentence): validate quotation of master072

--- tests/sentences/test_validate.py
+++ tests/sentences/test_validate.py
@@ -28,6 +28,7 @@ param = pytest.param
 
 @pytest.mark.parametrize('source, pages, expected', [
     param(power.BOOK173_PDF, '13:30', 'book173', id='book173'),
+    param(power.MASTER072_PDF, '13:18', 'master072', id='master072'),
 ])
 @utilatest.nightly
 def test_sentence_validate(source, pages, expected, testdir, monkeypatch):
