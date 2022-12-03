b3a800cb04cf2c836bd5e35e6dbf92f890d5c4a6
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-12-03 17:51:54 +0000

    chore(setup): use quick installer

--- setup.py
+++ setup.py
@@ -8,47 +8,19 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'words/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, 'requirements.txt'), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'words',
+    'words.abbreviation',
+    'words.feature',
+    'words.links',
+    'words.sentences',
+    'words.text',
+]
+ENTRY_POINTS = {
+    'console_scripts': ['words = words.cli:main'],
+}
 
 if __name__ == "__main__":
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='to be or not to be',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='words',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/words',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'words',
-            'words.abbreviation',
-            'words.feature',
-            'words.links',
-            'words.sentences',
-            'words.text',
-        ],
-        entry_points={
-            'console_scripts': ['words = words.cli:main'],
-        },
-    )
+    utila.install(__file__)
